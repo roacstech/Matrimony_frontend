a663a4aa5cac2cc4c51d752da65af50b63cafc37
vinothroacs <vinoth@roacs.com>
2026-02-14 11:20:22 +0530

    API live link changed all files

--- src/components/user/MyConnection.jsx
+++ src/components/user/MyConnection.jsx
@@ -257,7 +257,7 @@ useEffect(() => {
                 <div className="pt-2">
                   {selectedUser.horoscope_uploaded ? (
                     <button
-                      onClick={() => window.open(`http://localhost:5000/uploads/photos/${selectedUser.horoscope_file_name}`, "_blank")}
+                      onClick={() => window.open(`https://bec-frontend-matrimony-api.3t5o2t.easypanel.host/api/uploads/photos/${selectedUser.horoscope_file_name}`, "_blank")}
                       className="bg-[#3B1E54] text-white px-8 py-3 rounded-full text-[10px] font-black uppercase tracking-widest shadow-md hover:bg-[#2A153D] transition-all"
                     >
                       View Full Horoscope
--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -126,7 +126,7 @@ const Profile = () => {
             <div className="w-24 h-24 rounded-[30px] border-4 border-[#FAF6F3] overflow-hidden bg-gray-50 flex items-center justify-center shadow-md">
               {user?.photo ? (
                 <img
-                  src={`http://localhost:5000/uploads/photos/${user?.photo}`}
+                  src={`https://bec-frontend-matrimony-api.3t5o2t.easypanel.host/api/uploads/photos/${user?.photo}`}
                   className="w-full h-full object-cover"
                 />
               ) : (
@@ -302,7 +302,7 @@ const Profile = () => {
                 <FileText size={14} className="text-[#A67C52]" />
                 {user?.horoscope?.uploaded ? (
   <a
-              href={`http://localhost:5000/uploads/photos/${user?.horoscope?.fileUrl}`}
+              href={`https://bec-frontend-matrimony-api.3t5o2t.easypanel.host/api/uploads/photos/${user?.horoscope?.fileUrl}`}
     target="_blank"
     rel="noreferrer"
     className="underline"
