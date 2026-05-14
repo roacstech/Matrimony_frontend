e261c582ceb99ca7c8c75e2c8324cbc1c6cf6304
roacs <roacstech@gmail.com>
2026-05-14 17:51:39 +0530

    remove userroutes file

--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -1,28 +1,31 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import ConnectionCard from "../components/user/ConnectionCard";
 import ReceivedConnections from "../components/user/ReceivedConnections";
 import SentConnections from "../components/user/SentConnections";
 import Profile from "../components/user/Profile";
 import Notifications from "../components/user/Notifications";
 import UserDashboard from "../components/user/UserDashboard";
-import React from "react";
 
 const UserRoutes = () => {
   return (
     <Routes>
       <Route path="dashboard" element={<UserDashboard />}>
-
-        {/* DEFAULT PAGE */}
         <Route index element={<ConnectionCard />} />
 
-        {/* CONNECTIONS */}
-        <Route path="my-connection/received" element={<ReceivedConnections />} />
-        <Route path="my-connection/sent" element={<SentConnections />} />
+        <Route
+          path="my-connection/received"
+          element={<ReceivedConnections />}
+        />
 
-        {/* OTHER PAGES */}
+        <Route
+          path="my-connection/sent"
+          element={<SentConnections />}
+        />
+
         <Route path="profile" element={<Profile />} />
         <Route path="notifications" element={<Notifications />} />
-
       </Route>
     </Routes>
   );
